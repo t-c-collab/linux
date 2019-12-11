6709ac8aaad2dd24f9fc395725a7ba3b751c8cb4
Yuti Amonkar <yamonkar@cadence.com>
2019-12-11 15:10:11 +0530

    phy: Add DisplayPort configuration options
    Add generic DP API for configuring DisplayPort PHYs. The parameters
    that will be configured are link rate, number of lanes, voltage swing
    and pre-emphasis.
    
    Signed-off-by: Yuti Amonkar <yamonkar@cadence.com>

--- include/linux/phy/phy.h
+++ include/linux/phy/phy.h
@@ -16,6 +16,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
 
+#include <linux/phy/phy-dp.h>
 #include <linux/phy/phy-mipi-dphy.h>
 
 struct phy;
@@ -46,9 +47,12 @@ enum phy_mode {
  *
  * @mipi_dphy:	Configuration set applicable for phys supporting
  *		the MIPI_DPHY phy mode.
+ * @dp:		Configuration set applicable for phys supporting
+ *		the DisplayPort protocol.
  */
 union phy_configure_opts {
 	struct phy_configure_opts_mipi_dphy	mipi_dphy;
+	struct phy_configure_opts_dp		dp;
 };
 
 /**
